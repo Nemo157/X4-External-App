c45a8eb4a793df27e9ae1e3ca8715b9af2901d51
Wim Looman <git@nemo157.com>
2023-09-15 00:49:45 +0200

    Reduce compact mode padding around widget header and body

--- src/widgets/Widget.vue
+++ src/widgets/Widget.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="card" :class="[ compact ? 'mt-1' : 'mt-4' ]">
-    <div class="card-header">
+    <div class="card-header" :class="[ compact ? 'p-2' : 'p-4' ]">
       <slot name="header"></slot>
     </div>
-    <div class="card-body pt-0">
+    <div class="card-body pt-0" :class="[ compact ? 'p-2' : 'p-4' ]">
       <slot name="default"></slot>
     </div>
   </div>
